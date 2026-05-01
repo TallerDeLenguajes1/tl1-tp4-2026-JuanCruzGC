8f0d2d4505ab83c08682f85b598322c9e877a793
Juan Cruz <juangomezcorbalan@gmail.com>
2026-05-01 20:07:22 -0300

    Se añadio la funcion de marcarCompleta, la cual pide un id de tarea pendiente y la marca como completa

--- To-do_List.c
+++ To-do_List.c
@@ -18,15 +18,32 @@ typedef struct Nodo{
 //-----DECLARACION DE FUNCIONES-----
 void crearTarea(Nodo** tareas);
 void mostrarTareas(Nodo* tareas);
+void marcarCompletada(Nodo** pendientes,Nodo** completada, int id);
 //-----MAIN-----
 int main(){
     srand(time(NULL));
     Nodo* pendientes;
-    //Nodo* completadas;
+    Nodo* completadas;
     pendientes=NULL;
-    //completadas=NULL;
-    crearTarea(&pendientes);
+    completadas=NULL;
+    int respuesta=1,id;
+    while(respuesta==1){
+        crearTarea(&pendientes);
+        printf("Desea agregar otra tarea?   1:Si\n");
+        scanf("%d",&respuesta);
+        while(getchar()!='\n');
+    }
+    printf("-----Pendientes-----\n");
     mostrarTareas(pendientes);
+    printf("-----Completadas-----\n");
+    mostrarTareas(completadas);
+    printf("Ingrese el ID de la tarea para marcar como completa\n");
+    scanf("%d",&id);
+    marcarCompletada(&pendientes,&completadas,id);
+    printf("-----Pendientes-----\n");
+    mostrarTareas(pendientes);
+    printf("-----Completadas-----\n");
+    mostrarTareas(completadas);
     return 0;
 }
 //-----DEFINICION DE FUNCIONES-----
@@ -41,34 +58,69 @@ void crearTarea(Nodo** tareas){
         aux->T.duracion=10+rand()%(91);
         printf("Ingrese una descripcion de la tarea\n");
         gets(descr);
-        aux->T.descripcion=(char*)malloc(sizeof(char)*strlen(descr));   
+        aux->T.descripcion=(char*)malloc(sizeof(char)*strlen(descr)+1);   
         strcpy(aux->T.descripcion,descr);
         (*tareas)=aux;
     }else{
         Nodo* aux;
         aux=(Nodo*)malloc(sizeof(Nodo));
         aux->siguiente=*tareas;
-        aux->T.tareaID=(*tareas)->T.tareaID+1;
+        aux->T.tareaID=((*tareas)->T.tareaID)+1;
         aux->T.duracion=10+rand()%(91);
         printf("Ingrese una descripcion de la tarea\n");
         gets(descr);
-        aux->T.descripcion=(char*)malloc(sizeof(char)*strlen(descr)); 
+        aux->T.descripcion=(char*)malloc(sizeof(char)*strlen(descr)+1); 
         strcpy(aux->T.descripcion,descr);
         (*tareas)=aux;
     }
 }
 
 void mostrarTareas(Nodo* tareas){
-    Nodo* aux;
-    aux=tareas->siguiente;
-    printf("-----TAREAS PENDIENTES-----\n");
     while(tareas!=NULL){
-        printf("id: %d\n",tareas->T.tareaID);
+        printf("--ID: %d\n",tareas->T.tareaID);
         printf("Descripcion: %s",tareas->T.descripcion);
         printf("\nDuracion: %d\n",tareas->T.duracion);
-        tareas=aux;
-        aux=tareas->siguiente;
+        tareas=tareas->siguiente;
     }
+}
+
+void marcarCompletada(Nodo** pendientes,Nodo** completada,int id){
+    Nodo* aux1;
+    Nodo* aux2;
+    aux1=*pendientes;
+    aux2=aux1->siguiente;
+    int seEncontro;
+    seEncontro=0;
+    //Caso especial, la lista de pendientes solo tiene 1 o 0 elementos
+    if(aux2==NULL){
+        if(aux1->T.tareaID==id){
+            Nodo* aux3;
+            aux3=*completada;
+            *completada=aux1;
+            *pendientes=aux2;
+            aux1=aux3;
+            seEncontro=1;
+        }else{
+            if(aux1==NULL){
+                printf("La lista esta vacía...\n");
+            }
+        }
+    }else{
+        while(aux2!=NULL && seEncontro==0){
+            if(aux2->T.tareaID==id){
+                Nodo* aux3;
+                aux3=*completada;
+                *completada=aux2;
+                aux1->siguiente=aux2->siguiente;
+                aux2->siguiente=aux3;
+                seEncontro=1;
+            }
+            aux1=aux2;
+            aux2=aux1->siguiente;
+        }
+    }
+    if(seEncontro==1)   printf("El nodo se movio a tareas completadas correctamente\n");
+    else    printf("No se encontraron tareas con ese id...\n");
 }
 
 /*
